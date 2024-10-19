e5cc6427cb27d6d7093e79f3ab060c7d0b6ee8ec
abbosbek <abbosarabboyev9@gmail.com>
2024-10-19 14:30:55 +0500

    soem ui changes

---
+++ app/components/ModalContent/ModalContent.tsx
@@ -0,0 +1,50 @@
+import { type FC } from "react"
+import { TitleBar, Modal } from "@shopify/app-bridge-react"
+import { TextField, Checkbox, Thumbnail, Text } from "@shopify/polaris"
+import ImageIcon from '../../assets/images/ImageIcon.svg'
+
+interface ModalProps {
+    openModal: boolean
+    checked: boolean
+    handleChange: (newChecked: boolean) => void
+    setChecked: (newChecked: boolean) => void
+}
+
+const ModalContent: FC<ModalProps> = ({ openModal, checked, handleChange, setChecked }) => {
+
+    const handleItemClick = () => {
+        console.log("enviroments", process.env)
+        setChecked(!checked)
+    }
+    return (
+        <Modal open={openModal} id="my-modal">
+            <TitleBar title="Select product">
+                <button variant="primary" disabled>Select</button>
+                <button onClick={() => shopify.modal.hide('my-modal')}>Cancel</button>
+            </TitleBar>
+            <div className="modal-container">
+                <div className="search-bar">
+                    <TextField label="" placeholder="Search Products" onChange={() => { }} autoComplete="off" />
+                </div>
+                <div className="product-list">
+                    <div className="product-item" onClick={handleItemClick}>
+                        <Checkbox
+                            label=""
+                            checked={checked}
+                            onChange={handleChange}
+                        />
+                        <Thumbnail
+                            size="small"
+                            source={ImageIcon}
+                            alt=""
+                        />
+                        <Text variant="bodyMd" as="p">
+                            T-Shirt
+                        </Text>
+                    </div>
+                </div>
+            </div>
+        </Modal>
+    )
+}
+export default ModalContent
--- app/routes/app.related-products-app.tsx
+++ app/routes/app.related-products-app.tsx
@@ -1,33 +1,32 @@
-import { Modal, TitleBar } from "@shopify/app-bridge-react"
-import { Page, TextField } from "@shopify/polaris"
-import { useState } from "react"
+import { Page, Text } from "@shopify/polaris"
+import ModalContent from "app/components/ModalContent/ModalContent"
+import { useCallback, useState } from "react"
+import { authenticate } from "app/shopify.server"
+
 
 export default function Products() {
     const [openModal, setOpenModal] = useState(false)
+    const [checked, setChecked] = useState(false)
 
     const handleOpenModal = () => {
         setOpenModal(!openModal)
     }
+    console.log('authenticate', authenticate)
+
+    const handleChange = useCallback(
+        (newChecked: boolean) => setChecked(newChecked),
+        [],
+    )
 
     return (
         <div className="related-app-container">
             <Page title="Related Products" fullWidth>
-                <div className="relaated-product-content">
+                <div className="related-product-content">
                     <img src="https://cdn.shopify.com/s/files/1/2376/3301/products/emptystate-files.png" alt="empty" width="128" height="128" />
-                    <p className="paragraph">You do not have any related products</p>
+                    <Text as="p" variant="bodyMd">You do not have any related products</Text>
                     <button onClick={handleOpenModal} className="select-product">Select your First Product </button>
                 </div>
-
-                <Modal open={openModal} id="my-modal">
-                    <TitleBar title="Select product">
-                    <button variant="primary" disabled>Select</button>
-                    <button onClick={() => shopify.modal.hide('my-modal')}>Cancel</button>
-                    </TitleBar>
-                    <div className="modal-container">
-                        <TextField label="" placeholder="Search Products" onChange={() => {}} autoComplete="off" />
-                            <div className="product-list"></div>
-                    </div>
-                </Modal>
+                <ModalContent openModal={openModal} checked={checked} handleChange={handleChange} setChecked={setChecked} />
             </Page>
         </div>
     )
