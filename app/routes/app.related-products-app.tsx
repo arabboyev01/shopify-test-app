cc61cb7c5ef3f9422bb9652105669264c4d96d8b
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2024-10-20 23:02:17 +0500

    some changes

--- app/routes/app.related-products-app.tsx
+++ app/routes/app.related-products-app.tsx
@@ -1,19 +1,54 @@
-import { json, type LoaderFunctionArgs } from "@remix-run/node"
-import { useLoaderData } from "@remix-run/react"
+import { json, LoaderFunction, type LoaderFunctionArgs } from "@remix-run/node"
+import { useFetcher, useLoaderData } from "@remix-run/react"
 import { Page, Text } from "@shopify/polaris"
 import ModalContent from "app/components/ModalContent/ModalContent"
 import { authenticate } from "app/shopify.server"
 import { useCallback, useState } from "react"
 
+// export const loader = async ({ request }: LoaderFunctionArgs) => {
+//   const { session, redirect } = await authenticate.admin(request);
+//     return { session , redirect}
+// }
+
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-    await authenticate.admin(request)
-    console.log("Headers:", [...request.headers.entries()])
-    const apiKey = request.headers.get('x-api-key')
+    try {
+        const { admin } = await authenticate.admin(request)
+        if (admin) {
+            console.log('Admin is authenticated:', admin)
+            const response = await admin.graphql(
+                `#graphql
+                    query {
+                        products(first: 10) {
+                        edges {
+                            node {
+                            id
+                            title
+                            handle
+                            }
+                            cursor
+                        }
+                        pageInfo {
+                            hasNextPage
+                        }
+                        }
+                    }`,
+            );
 
-    return json({ apiKey })
+            console.log('Products Data:', response)
+            if (!response.ok) {
+                throw new Error(`Failed to fetch products: ${response.statusText}`);
+            }
 
+            const data = await response.json()
+
+            return { products: data }
+        } else {
+            throw new Error('Admin authentication failed.');
+        }
+    } catch (error: unknown) {
+        return { error: (error as Error).message || "An unexpected error occurred." }
+    }
 }
-
 
 export default function Products() {
     const [openModal, setOpenModal] = useState(false)
