9d86117ccfc57fe265da57cfe3c7c6b9d926e3d2
Khurshidbek Sobirov <xurshidbeksobirov03@gmail.com>
2024-10-24 17:32:18 +0500

    some changes

--- app/components/Setting/OnlineStore.tsx
+++ app/components/Setting/OnlineStore.tsx
@@ -1,13 +1,22 @@
 import { Card, Page } from "@shopify/polaris"
+import { TextField } from '@shopify/polaris'
 
 export default function OnlineStore() {
-  
+
     return (
         <Page fullWidth>
             <Card padding={{ xs: "600" }}>
                 <h1 className="sync-header">AJAX cart callback function (for developers)</h1>
-                <p className="sync-description" style={{ marginTop: "20px"}}>Enter your code to trigger ajax cart</p>
-                <button className="manage-languages">Save</button>
+                <div className="text-field">
+                    <TextField
+                        label="Enter your code to trigger ajax cart"
+                        value={""}
+                        onChange={console.log}
+                        multiline={6}
+                        autoComplete="off"
+                    />
+                </div>
+                <button className="select-product">Save</button>
             </Card>
         </Page>
 
